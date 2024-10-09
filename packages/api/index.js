242172488b9ab0e7b1cadf2e4d7ce8e3a4b00bd1
fathiraz arthuro <arthurofathiraz@gmail.com>
2024-10-09 11:34:44 +0700

    fix(api): build in vercel

--- packages/api/index.js
+++ packages/api/index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 // Enable CORS for all routes
 app.use(cors());
 
+// Serve root 
+app.get('/', (req, res) => {
+    res.send(`Server is running on port ${PORT}`)
+});
+
 // Serve resume.json
 app.get('/api/resume', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/resume.json'));
@@ -17,9 +22,6 @@ app.get('/api/config', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/config.json'));
 });
 
-// running only not in vercel
-if (process.env.VERCEL_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
